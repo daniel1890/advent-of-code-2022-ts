08a7430d8de3cd5d1e658c6a13db724e04d5b5b3
Daniël <d.roth@student.han.nl>
2022-12-05 23:05:51 +0100

    day 5 part 2 done, need to refactor

--- src/day-5/day_5.ts
+++ src/day-5/day_5.ts
@@ -7,7 +7,7 @@ const stacksLines: string[] = stacksString.split("\n");
 
 let answers = {
     answer1: '',
-    answer2: 0
+    answer2: ''
 };
 
 const instructions: number[][] = splitInput[1].split("\n").map(
@@ -22,6 +22,8 @@ const instructions: number[][] = splitInput[1].split("\n").map(
 );
 
 let stacks = new Array(9).fill(0).map(() => new Array(0).fill(0));
+let stacks2 = new Array(9).fill(0).map(() => new Array(0).fill(0));
+
 
 for (let i = 0; i < stacksLines.length; i++){
   let line = stacksLines[i];
@@ -30,6 +32,7 @@ for (let i = 0; i < stacksLines.length; i++){
         let stackIndex = j + 1 + (j * 3)
         if(line[stackIndex] != ' ') {
             stacks[j].push(line.slice(stackIndex, stackIndex + 2).split(']')[0]);
+            stacks2[j].push(line.slice(stackIndex, stackIndex + 2).split(']')[0]);
         }
     }
 }
@@ -37,38 +40,70 @@ for (let i = 0; i < stacksLines.length; i++){
 stacks.map(
     stack => stack.reverse().shift()
 );
+stacks2.map(
+    stack => stack.reverse().shift()
+);
 
+// SOLUTION FOR PART 1
+const solvePartOne = (stacks: any[][], instructions: number[][]) => {
+    let answer = '';
+    for (let i = 0; i < instructions.length; i++){
+        let instruction = instructions[i];
+        let stackToRemove: string[] = [];
 
-for (let i = 0; i < instructions.length; i++){
-    let instruction = instructions[i];
-    let stackToRemove: string[] = [];
-    console.log({instruction});
+        for(let j = 0; j < instruction[0]; j++) {
+            let removedElementFromStack = stacks[instruction[1] - 1].pop();
+            stackToRemove.push(removedElementFromStack);
+        }
 
-    for(let j = 0; j < instruction[0]; j++) {
-        let removedElementFromStack = stacks[instruction[1] - 1].pop();
-        stackToRemove.push(removedElementFromStack);
+        for(let k = 0; k < (instruction[0] - 1); k++) {
+            stacks[instruction[2] - 1].push(stackToRemove[k])
+        }
+
+        stacks[instruction[2] - 1].push(stackToRemove[instruction[0] - 1]);
     }
-    console.log({stackToRemove});
-    console.log("stack removed from " + stacks[instruction[1] - 1])
 
-    for(let k = 0; k < (instruction[0] - 1); k++) {
-        stacks[instruction[2] - 1].push(stackToRemove[k])
-        console.log(stackToRemove[k]);
-    }
+    stacks.forEach(
+        stack => {
+            answer += stack[stack.length - 1]; 
+        }
+    )
 
-    stacks[instruction[2] - 1].push(stackToRemove[instruction[0] - 1]);
-    console.log("stack added to " +stacks[instruction[2] - 1])
-
+    return answer;
 }
 
-stacks.forEach(
-    stack => {
-        answers.answer1 += stack[stack.length - 1]; 
+// SOLUTION FOR PART 2
+const solvePartTwo = (stacks: any[][], instructions: number[][]) => {
+    let answer = '';
+    for (let i = 0; i < instructions.length; i++){
+        let instruction = instructions[i];
+        let stackToRemove: string[] = [];
+
+        for(let j = 0; j < instruction[0]; j++) {
+            let removedElementFromStack = stacks[instruction[1] - 1].pop();
+            stackToRemove.push(removedElementFromStack);
+        }
+
+        for(let k = (instruction[0] - 1); k > 0; k--) {
+            stacks[instruction[2] - 1].push(stackToRemove[k])
+        }
+
+        stacks[instruction[2] - 1].push(stackToRemove[0]);
     }
-)
+
+    stacks.forEach(
+        stack => {
+            answer += stack[stack.length - 1]; 
+        }
+    )
+
+    return answer;
+}
+
+answers.answer1 = solvePartOne(stacks, instructions);
+answers.answer2 = solvePartTwo(stacks2, instructions);
 
 console.log(stacks)
-
 console.log(answers);
 
 // npx ts-node src/day-5/day_5.ts
